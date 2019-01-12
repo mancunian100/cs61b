23f42f49899d4021d522d6212b42348b9f0f996c
Jack Chen <876209107@qq.com>
2019-01-12 21:46:05 +0800

    modify resise

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -8,9 +8,9 @@ public class ArrayDeque<T> {
     /** the size of array. */
     private int size;
     /** the pointer of the new first Item. */
-    private int nextFirst;
+    public int nextFirst;
     /** the pointer of the new last Item. */
-    private int nextLast;
+    public int nextLast;
 
     /** create an empty item array. */
     public ArrayDeque() {
@@ -23,19 +23,29 @@ public class ArrayDeque<T> {
     /** there some bugs in it, need to be fixed */
     private void resize(int s) {
         T [] newItems = (T []) new Object[s];
-        int l = newItems.length;
-        if (nextFirst >= l) {
-            nextFirst -= l;
-        }
-        if (nextLast >= l) {
-            nextLast -= l;
-        }
+        int lOld = items.length;
+        int lNew = newItems.length;
         for (int i = 0; i < size; i++) {
-            if (nextFirst + 1 + i >= l) {
-                newItems[nextFirst + 1 + i - l] = items[nextFirst + 1 + i];
+            if (nextFirst + 1 + i >= lNew) {
+                if (nextFirst + 1 + i >= lOld) {
+                    newItems[nextFirst + 1 + i - lNew] = items[nextFirst + 1 + i - lOld];
+                } else {
+                    newItems[nextFirst + 1 + i - lNew] = items[nextFirst + 1 + i];
+                }
             } else {
-                newItems[nextFirst + 1 + i] = items[nextFirst + 1 + i];
+                if (nextFirst + 1 + i >= lOld) {
+                    newItems[nextFirst + 1 + i] = items[nextFirst + 1 + i - lOld];
+                } else {
+                    newItems[nextFirst + 1 + i] = items[nextFirst + 1 + i];
+                }
             }
+        }
+        if (nextFirst + 1 > lNew) {
+            nextFirst -= lNew;
+        }
+        nextLast = nextFirst + size + 1;
+        if (nextLast + 1 > lNew) {
+            nextLast -= lNew;
         }
         items = newItems;
     }
@@ -90,11 +100,11 @@ public class ArrayDeque<T> {
     /** print all the Items in the array, separated by a space. */
     public void printDeque() {
         for (int i = 0; i < size; i++) {
-            int index = i + nextFirst;
+            int index = nextFirst + i + 1;
             if (index >= items.length) {
                 index -= items.length;
+                System.out.print(items[index] + " ");
             }
-            System.out.print(items[index] + " ");
         }
     }
 
@@ -106,7 +116,7 @@ public class ArrayDeque<T> {
             return null;
         }
         size -= 1;
-        if (items.length > 8 && size < 0.25 * items.length) {
+        if (items.length > 15 && size < 0.25 * items.length) {
             resize((int) (items.length * 0.5));
         }
         int index = nextFirst + 1;
@@ -127,7 +137,7 @@ public class ArrayDeque<T> {
             return null;
         }
         size -= 1;
-        if (items.length > 8 && size < 0.25 * items.length) {
+        if (items.length > 15 && size < 0.25 * items.length) {
             resize((int) (items.length * 0.5));
         }
         T oldLast = items[nextLast - 1];
@@ -143,7 +153,7 @@ public class ArrayDeque<T> {
         if (i >= size) {
             return null;
         }
-        int index = nextFirst + 1 + 1;
+        int index = nextFirst + 1 + i;
         if (index >= items.length) {
             index -= items.length;
         }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,88 +1,46 @@
 /** Performs some basic array list tests. */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class ArrayDequeTest {
 
-    /* Utility method for printing out empty checks. */
-    public static boolean checkEmpty(boolean expected, boolean actual) {
-		if (expected != actual) {
-			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
-			return false;
-		}
-		return true;
-	}
+    /** test add, size and get. */
+    @Test
+    public void testAddGet() {
+        ArrayDeque<String> ad1 = new ArrayDeque<>();
+        ad1.addFirst("Hello");
+        assertEquals("Hello", ad1.get(0));
+        assertEquals(1, ad1.size());
+    }
 
-	/* Utility method for printing out empty checks. */
-	public static boolean checkSize(int expected, int actual) {
-		if (expected != actual) {
-			System.out.println("size() returned " + actual + ", but expected: " + expected);
-			return false;
-		}
-		return true;
-	}
+    /** test remove. */
+    @Test
+    public void testRemove() {
+        ArrayDeque<String> ad1 = new ArrayDeque<>();
+        ad1.addLast("H");
+        ad1.printDeque();
+        ad1.removeFirst();
+        assertEquals(0, ad1.size());
+        ad1.addFirst("o");
+        assertEquals("o", ad1.get(0));
+        ad1.removeLast();
+        assertEquals(true, ad1.isEmpty());
+    }
 
-	/* Prints a nice message based on whether a test passed.
-	 * The \n means newline. */
-	public static void printTestStatus(boolean passed) {
-		if (passed) {
-			System.out.println("Test passed!\n");
-		} else {
-			System.out.println("Test failed!\n");
-		}
-	}
-
-	/** Adds a few things to the list, checking isEmpty() and size() are correct,
-	 * finally printing the results.
-	 *
-	 * && is the "and" operation. */
-	public static void addIsEmptySizeTest() {
-		System.out.println("Running add/isEmpty/Size test.");
-
-		ArrayDeque<String> ad1 = new ArrayDeque<String>();
-
-		boolean passed = checkEmpty(true, ad1.isEmpty());
-
-		ad1.addFirst("front");
-
-		// The && operator is the same as "and" in Python.
-		// It's a binary operator that returns true if both arguments true, and false otherwise.
-		passed = checkSize(1, ad1.size()) && passed;
-		passed = checkEmpty(false, ad1.isEmpty()) && passed;
-
-		ad1.addLast("middle");
-		passed = checkSize(2, ad1.size()) && passed;
-
-		ad1.addLast("back");
-		passed = checkSize(3, ad1.size()) && passed;
-
-		System.out.println("Printing out deque: ");
-		ad1.printDeque();
-
-		printTestStatus(passed);
-	}
-
-	/** Adds an item, then removes an item, and ensures that ad is empty afterwards. */
-	public static void addRemoveTest() {
-
-		System.out.println("Running add/remove test.");
-
-		ArrayDeque<Integer> ad1 = new ArrayDeque<>();
-		// should be empty
-		boolean passed = checkEmpty(true, ad1.isEmpty());
-
-		ad1.addFirst(10);
-		// should not be empty
-		passed = checkEmpty(false, ad1.isEmpty()) && passed;
-
-		ad1.removeFirst();
-		// should be empty
-		passed = checkEmpty(true, ad1.isEmpty()) && passed;
-
-		printTestStatus(passed);
-
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Running tests.\n");
-		addIsEmptySizeTest();
-		addRemoveTest();
-	}
+    /** test resize. */
+    @Test
+    public void testResize() {
+        ArrayDeque<String> ad2 = new ArrayDeque<>();
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+        ad2.addFirst("h");
+	    System.out.println(ad2.nextFirst);
+    }
 }
