e724c7363da5b095fec40f3991476e8bf668769a
Jack Chen <876209107@qq.com>
2019-01-15 16:12:54 +0800

    modify proj1b

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -3,14 +3,14 @@ import static org.junit.Assert.*;
 
 public class TestOffByN {
 
-	static CharacterComparator offby5 = new OffByN(5);
+    static CharacterComparator offby5 = new OffByN(5);
 
-	@Test
-	public void testOffByN() {
+    @Test
+    public void testOffByN() {
         char a = 'a';
         char b = 'f';
-		boolean expected = true;
-		boolean actual = offby5.equalChars(a, b);
-		assertEquals(expected, actual);
-	}
+        boolean expected = true;
+        boolean actual = offby5.equalChars(a, b);
+        assertEquals(expected, actual);
+    }
 }
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,11 +10,21 @@ public class TestOffByOne {
     // Your tests go here.
     @Test
     public void testOffByOne() {
-        char a = 'f';
-        char b = 'g';
-        boolean actual = offByOne.equalChars(a, b);
-        boolean expected = true;
-        assertEquals(expected, actual);
+        char a1 = 'F';
+        char b1 = 'G';
+        char a2 = '&';
+        char b2 = '%';
+        char a3 = 'c';
+        char b3 = 'd';
+        boolean actual1 = offByOne.equalChars(a1, b1);
+        boolean actual2 = offByOne.equalChars(a2, b2);
+        boolean actual3 = offByOne.equalChars(a3, b3);
+        boolean expected1 = true;
+        boolean expected2 = true;
+        boolean expected3 = true;
+        assertEquals(expected1, actual1);
+        assertEquals(expected2, actual2);
+        assertEquals(expected3, actual3);
     }
 //    Uncomment this class once you've created your
 //    CharacterComparator interface and OffByOne class.
