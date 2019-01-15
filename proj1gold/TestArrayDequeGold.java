69183025e32d383ce269ca184a4cab376fbf79a4
Jack Chen <876209107@qq.com>
2019-01-15 20:14:57 +0800

    add proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,42 @@
+/**
+ * Created by mancunian100 on 2019/01/15
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void testArrayDeque() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ad1 = new ArrayDequeSolution<>();
+
+        for (Integer i = 0; i < 10; i++) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+
+            if (numberBetweenZeroAndOne > 0.5) {
+                sad1.addFirst(i);
+                ad1.addFirst(i);
+            } else {
+                sad1.addLast(i);
+                ad1.addLast(i);
+            }
+        }
+
+        for (Integer i = 0; i < 10; i++) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+
+            Integer s;
+            Integer a;
+            if (numberBetweenZeroAndOne > 0.5) {
+                s = sad1.removeFirst();
+                a = ad1.removeFirst();
+            } else {
+                s = sad1.removeLast();
+                a = ad1.removeLast();
+            }
+            assertEquals("addFirst(), addLast()", a, s);
+        }
+    }
+}
