7cbfea1c6a12dca01b94c11eecb391fb66c9a85f
Jack Chen <876209107@qq.com>
2019-01-15 20:34:03 +0800

    modify proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -36,7 +36,7 @@ public class TestArrayDequeGold {
                 s = sad1.removeLast();
                 a = ad1.removeLast();
             }
-            assertEquals("removeFirst(), removeLast()", a, s);
+            assertEquals("removeFirst()", a, s);
         }
     }
 
