6256d54c8c2e26af5802580bfab0a7633e49fc3e
Jack Chen <876209107@qq.com>
2019-02-28 02:13:57 +0800

    add hw3

---
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -0,0 +1,29 @@
+package hw3.hash;
+
+import java.util.List;
+
+public class OomageTestUtility {
+    public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
+        /* TODO:
+         * Write a utility function that returns true if the given oomages
+         * have hashCodes that would distribute them fairly evenly across
+         * M buckets. To do this, convert each oomage's hashcode in the
+         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
+         * and ensure that no bucket has fewer than N / 50
+         * Oomages and no bucket has more than N / 2.5 Oomages.
+         */
+        int[] buckets = new int[M];
+        int N = 0;
+        for (Oomage oomage : oomages) {
+            int bucketNum = (oomage.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum] += 1;
+            N += 1;
+        }
+        for (int i : buckets) {
+            if ((i <= N / 50) || (i >= N / 2.5)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -0,0 +1,82 @@
+package hw3.hash;
+
+import org.junit.Test;
+
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class TestSimpleOomage {
+
+    @Test
+    public void testHashCodeDeterministic() {
+        SimpleOomage so = SimpleOomage.randomSimpleOomage();
+        int hashCode = so.hashCode();
+        for (int i = 0; i < 100; i += 1) {
+            assertEquals(hashCode, so.hashCode());
+        }
+    }
+
+    @Test
+    public void testHashCodePerfect() {
+        /* TODO: Write a test that ensures the hashCode is perfect,
+          meaning no two SimpleOomages should EVER have the same
+          hashCode UNLESS they have the same red, blue, and green values!
+         */
+        HashSet<Integer> hashSimpleOomage = new HashSet<>();
+        for (int i = 0; i < 51; i += 1) {
+            for (int j = 0; j < 51; j += 1) {
+                for (int k = 0; k < 51; k += 1) {
+                    SimpleOomage oijk = new SimpleOomage(i * 5, j * 5, k * 5);
+                    Integer hashCode = oijk.hashCode();
+                    assertFalse(hashSimpleOomage.contains(hashCode));
+                    hashSimpleOomage.add(hashCode);
+                }
+            }
+        }
+
+    }
+
+    @Test
+    public void testEquals() {
+        SimpleOomage ooA = new SimpleOomage(5, 10, 20);
+        SimpleOomage ooA2 = new SimpleOomage(5, 10, 20);
+        SimpleOomage ooB = new SimpleOomage(50, 50, 50);
+        assertEquals(ooA, ooA2);
+        assertNotEquals(ooA, ooB);
+        assertNotEquals(ooA2, ooB);
+        assertNotEquals(ooA, "ketchup");
+    }
+
+
+    @Test
+    public void testHashCodeAndEqualsConsistency() {
+        SimpleOomage ooA = new SimpleOomage(5, 10, 20);
+        SimpleOomage ooA2 = new SimpleOomage(5, 10, 20);
+        HashSet<SimpleOomage> hashSet = new HashSet<>();
+        hashSet.add(ooA);
+        assertTrue(hashSet.contains(ooA2));
+    }
+
+    @Test
+    public void testRandomOomagesHashCodeSpread() {
+        List<Oomage> oomages = new ArrayList<>();
+        int N = 10000;
+
+        for (int i = 0; i < N; i += 1) {
+            oomages.add(SimpleOomage.randomSimpleOomage());
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
+    }
+
+    /** Calls tests for SimpleOomage. */
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestSimpleOomage.class);
+    }
+}
