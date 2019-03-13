3f6eff3ab1ad9d332dc53b0b3f9db3abdc1fc80e
Jack Chen <876209107@qq.com>
2019-03-13 16:53:52 +0800

    finish part1 raster

--- proj3/src/main/java/GraphBuildingHandler.java
+++ proj3/src/main/java/GraphBuildingHandler.java
@@ -68,9 +68,9 @@ public class GraphBuildingHandler extends DefaultHandler {
         if (qName.equals("node")) {
             /* We encountered a new <node...> tag. */
             activeState = "node";
-//            System.out.println("Node id: " + attributes.getValue("id"));
-//            System.out.println("Node lon: " + attributes.getValue("lon"));
-//            System.out.println("Node lat: " + attributes.getValue("lat"));
+            System.out.println("Node id: " + attributes.getValue("id"));
+            System.out.println("Node lon: " + attributes.getValue("lon"));
+            System.out.println("Node lat: " + attributes.getValue("lat"));
 
             /* TODO Use the above information to save a "node" to somewhere. */
             /* Hint: A graph-like structure would be nice. */
@@ -78,10 +78,10 @@ public class GraphBuildingHandler extends DefaultHandler {
         } else if (qName.equals("way")) {
             /* We encountered a new <way...> tag. */
             activeState = "way";
-//            System.out.println("Beginning a way...");
+            System.out.println("Beginning a way...");
         } else if (activeState.equals("way") && qName.equals("nd")) {
             /* While looking at a way, we found a <nd...> tag. */
-            //System.out.println("Id of a node in this way: " + attributes.getValue("ref"));
+            System.out.println("Id of a node in this way: " + attributes.getValue("ref"));
 
             /* TODO Use the above id to make "possible" connections between the nodes in this way */
             /* Hint1: It would be useful to remember what was the last node in this way. */
@@ -98,13 +98,13 @@ public class GraphBuildingHandler extends DefaultHandler {
                 //System.out.println("Max Speed: " + v);
                 /* TODO set the max speed of the "current way" here. */
             } else if (k.equals("highway")) {
-                //System.out.println("Highway type: " + v);
+                System.out.println("Highway type: " + v);
                 /* TODO Figure out whether this way and its connections are valid. */
                 /* Hint: Setting a "flag" is good enough! */
             } else if (k.equals("name")) {
-                //System.out.println("Way Name: " + v);
+                System.out.println("Way Name: " + v);
             }
-//            System.out.println("Tag with k=" + k + ", v=" + v + ".");
+            System.out.println("Tag with k=" + k + ", v=" + v + ".");
         } else if (activeState.equals("node") && qName.equals("tag") && attributes.getValue("k")
                 .equals("name")) {
             /* While looking at a node, we found a <tag...> with k="name". */
@@ -112,7 +112,7 @@ public class GraphBuildingHandler extends DefaultHandler {
             /* Hint: Since we found this <tag...> INSIDE a node, we should probably remember which
             node this tag belongs to. Remember XML is parsed top-to-bottom, so probably it's the
             last node that you looked at (check the first if-case). */
-//            System.out.println("Node's name: " + attributes.getValue("v"));
+            System.out.println("Node's name: " + attributes.getValue("v"));
         }
     }
 
@@ -133,7 +133,7 @@ public class GraphBuildingHandler extends DefaultHandler {
             /* We are done looking at a way. (We finished looking at the nodes, speeds, etc...)*/
             /* Hint1: If you have stored the possible connections for this way, here's your
             chance to actually connect the nodes together if the way is valid. */
-//            System.out.println("Finishing a way...");
+            System.out.println("Finishing a way...");
         }
     }
 
--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import java.util.ArrayList;
+
 /**
  * This class provides all code necessary to take a query box and produce
  * a query result. The getMapRaster method must return a Map containing all
@@ -9,8 +11,23 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    private double[] lonDPPs;
+    private double[] latDPPs;
+    private double rootUllon = MapServer.ROOT_ULLON;
+    private double rootLrlon = MapServer.ROOT_LRLON;
+    private double rootUllat = MapServer.ROOT_ULLAT;
+    private double rootLrlat = MapServer.ROOT_LRLAT;
+
     public Rasterer() {
-        // YOUR CODE HERE
+        /** LonDPP for all levels of pics. */
+        lonDPPs = new double[8];
+        latDPPs = new double[8];
+        double rootLonDPP = (rootLrlon - rootUllon) / MapServer.TILE_SIZE;
+        double rootLatDPP = (rootUllat - rootLrlat) / MapServer.TILE_SIZE;
+        for (int i = 0; i <= 7; i += 1) {
+            lonDPPs[i] = rootLonDPP / (Math.pow(2, i));
+            latDPPs[i] = rootLatDPP / (Math.pow(2, i));
+        }
     }
 
     /**
@@ -42,11 +59,150 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+        /** read the params. */
+        double queryBoxUllon = params.get("ullon");
+        double queryBoxLrlon = params.get("lrlon");
+        double queryBoxWidth = params.get("w");
+
+        /** LonDPP of the query box. */
+        double queryBoxLonDPP = (queryBoxLrlon - queryBoxUllon) / queryBoxWidth;
+
+        /** depth of pictures. */
+        int depth = 0;
+
+        /** select the appropriate level of picture. */
+        for (; depth <= 7; depth += 1) {
+            if (depth == 7 || lonDPPs[depth] <= queryBoxLonDPP) {
+                break;
+            }
+        }
+
+        /** LonDPP selected. */
+        double resultLonDPP = lonDPPs[depth];
+        double resultLatDPP = latDPPs[depth];
+
+        /** select the grids of pictures. */
+        ArrayList<ArrayList<String>> grids = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> rcs = new ArrayList<>();
+        for (int r = 0; r < Math.pow(2, depth); r += 1) {
+            /** per raw of pictures. */
+            ArrayList<String> grid = new ArrayList<>();
+            for (int c = 0; c < Math.pow(2, depth); c += 1) {
+                double[][] vertices = getVertices(r, c, resultLonDPP, resultLatDPP);
+                if (decideCover(vertices, params)) {
+                    String g = "d" + depth + "_x" + c + "_y" + r + ".png";
+                    grid.add(g);
+                    ArrayList<Integer> rc = new ArrayList<>();
+                    rc.add(r);
+                    rc.add(c);
+                    rcs.add(rc);
+                }
+            }
+            if (grid.size() != 0) {
+                grids.add(grid);
+            }
+        }
+
+        /** the lon and lat of the grids. */
+        double ullon, ullat, lrlon, lrlat;
+        /** upper left vertex raster. */
+        int ulr = rcs.get(0).get(0);
+        int ulc = rcs.get(0).get(1);
+        double[][] ulVertex = getVertices(ulr, ulc, resultLonDPP, resultLatDPP);
+        ullon = ulVertex[0][0];
+        ullat = ulVertex[0][1];
+        /** lower right vertex raster. */
+        int len = rcs.size();
+        int lrr = rcs.get(len - 1).get(0);
+        int lrc = rcs.get(len - 1).get(1);
+        double[][] lrVertex = getVertices(lrr, lrc, resultLonDPP, resultLatDPP);
+        lrlon = lrVertex[1][0];
+        lrlat = lrVertex[1][1];
+        /** convert the String ArrayList to array. */
+        int rawOfString = grids.size();
+        int colOfString = grids.get(0).size();
+        String[][] renderGrids = new String[rawOfString][colOfString];
+
+        for (int r = 0; r < rawOfString; r += 1) {
+            for (int c = 0; c < colOfString; c += 1) {
+                String rc = grids.get(r).get(c);
+                renderGrids[r][c] = rc;
+            }
+        }
+        /** return the results. */
+        results.put("raster_ul_lon", ullon);
+        results.put("raster_ul_lat", ullat);
+        results.put("raster_lr_lon", lrlon);
+        results.put("raster_lr_lat", lrlat);
+        results.put("depth", depth);
+        results.put("render_grid", renderGrids);
+        results.put("query_success", true);
+        System.out.print(results);
         return results;
+    }
+
+    /** private helper method for getting the vertices of the given picture block.
+     *
+     * @param raw the raw if the given picture
+     * @param col the column of the given picture
+     * @param selectLonDPP the LonDPP if the given picture
+     * @return the lon and lat of the four vertices
+     */
+    private double[][] getVertices(int raw, int col, double selectLonDPP, double selectLatDPP) {
+        double[][] vertices = new double[2][2];
+        double left = rootUllon + (col * selectLonDPP * MapServer.TILE_SIZE);
+        double right = rootUllon + ((col + 1) * selectLonDPP * MapServer.TILE_SIZE);
+        double upper = rootUllat - (raw * selectLatDPP * MapServer.TILE_SIZE);
+        double lower = rootUllat - ((raw + 1) * selectLatDPP * MapServer.TILE_SIZE);
+        double[] vertex00 = {left, upper};
+        double[] vertex01 = {right, lower};
+        vertices[0] = vertex00;
+        vertices[1] = vertex01;
+        return vertices;
+    }
+
+    /** private helper method for determine if the picture is covered by query box.
+     *
+     * @param vertices four vertices of the picture.
+     * @param params the query box.
+     * @return true if query box covers the picture.
+     */
+    private boolean decideCover(double[][] vertices, Map<String, Double> params) {
+        /** query box. */
+        double qbleft = params.get("ullon");
+        double qbright = params.get("lrlon");
+        double qbupper = params.get("ullat");
+        double qblower = params.get("lrlat");
+
+        /** vertices of pictures. */
+        double[][] V = {
+                {vertices[0][0], vertices[0][1]},
+                {vertices[1][0], vertices[0][1]},
+                {vertices[0][0], vertices[1][1]},
+                {vertices[1][0], vertices[1][1]}
+        };
+
+        // condition1: the query box covers one of the vertices.
+        for (double[] v : V) {
+            if ((qbleft <= v[0] && v[0] <= qbright)
+                    && (qblower <= v[1] && v[1] <= qbupper)) {
+                return true;
+            }
+        }
+
+        // condition2: the four vertices could cover the query box.
+        boolean v1 = qbleft >= V[0][0] && qbupper <= V[0][1];
+        boolean v2 = qbright <= V[1][0] && qbupper <= V[1][1];
+        boolean v3 = qbleft >= V[2][0] && qblower >= V[2][1];
+        boolean v4 = qbright <= V[3][0] && qblower >= V[3][1];
+        if (v1 && v2 && v3 && v4) {
+            return true;
+        }
+
+        return false;
     }
 
 }
