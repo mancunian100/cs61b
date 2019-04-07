93b57eb8cd644d6d2ff11d12fb6473dd032fbe7b
Jack Chen <876209107@qq.com>
2019-04-07 23:37:48 +0800

    radix sort fix

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -15,18 +15,88 @@ public class RadixSort {
      *
      * @return String[] the sorted array
      */
+//    public static String[] sort(String[] asciis) {
+//        int len = 0;
+//        for (String str : asciis) {
+//            int l = str.length();
+//            len = l > len ? l : len;
+//        }
+//        String[] sorted = new String[asciis.length];
+//        System.arraycopy(asciis, 0, sorted, 0, asciis.length);
+//        sortHelperLSD(sorted, len);
+//        return sorted;
+//    }
+//
+//    /**
+//     * LSD helper method that performs a destructive counting sort the array of
+//     * Strings based off characters at a specific index.
+//     * @param asciis Input array of Strings
+//     * @param index The position to sort the Strings on.
+//     */
+//    private static void sortHelperLSD(String[] asciis, int index) {
+//        // Optional LSD helper method for required LSD radix sort
+//        if (index == 0) {
+//            return;
+//        }
+//        int[] counts = new int[256];
+//        String[] unsorted = new String[asciis.length];
+//        System.arraycopy(asciis, 0, unsorted, 0, asciis.length);
+////        String[] sorted = new String[asciis.length];
+//        int len = 0;
+//        for (String str : unsorted) {
+//            int l = str.length();
+//            len = l > len ? l : len;
+//        }
+//        for (String str : unsorted) {
+//            int at = index - (len - str.length()) - 1;
+//            int label;
+//            if (at < 0) {
+//                label = 0;
+//            } else {
+//                label = (int) str.charAt(at);
+//            }
+//            counts[label] += 1;
+//        }
+//        for (int i = 1; i < counts.length; i += 1) {
+//            counts[i] += counts[i - 1];
+//        }
+//        for (int i = unsorted.length - 1; i >= 0; i -= 1) {
+//            String str = unsorted[i];
+//            int at = index - (len - str.length()) - 1;
+//            int label;
+//            if (at < 0) {
+//                label = 0;
+//            } else {
+//                label = (int) str.charAt(at);
+//            }
+//            counts[label] -= 1;
+//            int place = counts[label];
+//            asciis[place] = str;
+//        }
+//        sortHelperLSD(asciis, index - 1);
+//    }
+
+
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
-        int len = 0;
-        for (String str : asciis) {
-            int l = str.length();
-            len = l > len ? l : len;
-        }
+
         String[] sorted = new String[asciis.length];
         System.arraycopy(asciis, 0, sorted, 0, asciis.length);
-        sortHelperLSD(sorted, len);
+
+        // get the maximum length of string in asciis
+        int max = 0;
+        for (String asc : asciis) {
+            max = max > asc.length() ? max : asc.length();
+        }
+
+        int index = 0;
+        while (index < max) {
+            sorted = sortHelperLSD(sorted, index);
+            index += 1;
+        }
+
         return sorted;
     }
+
 
     /**
      * LSD helper method that performs a destructive counting sort the array of
@@ -34,47 +104,46 @@ public class RadixSort {
      * @param asciis Input array of Strings
      * @param index The position to sort the Strings on.
      */
-    private static void sortHelperLSD(String[] asciis, int index) {
+    private static String[] sortHelperLSD(String[] asciis, int index) {
         // Optional LSD helper method for required LSD radix sort
-        if (index == 0) {
-            return;
+
+        // buckets for inserting number of "digits"
+        int[] buckets = new int[256];
+        String[] sorted = new String[asciis.length];
+
+        // counting process
+        for (String a : asciis) {
+            int pos = (int) a.charAt(a.length() - index - 1);
+            buckets[pos] += 1;
         }
-        int[] counts = new int[256];
-        String[] unsorted = new String[asciis.length];
-        System.arraycopy(asciis, 0, unsorted, 0, asciis.length);
-//        String[] sorted = new String[asciis.length];
-        int len = 0;
-        for (String str : unsorted) {
-            int l = str.length();
-            len = l > len ? l : len;
-        }
-        for (String str : unsorted) {
-            int at = index - (len - str.length()) - 1;
-            int label;
-            if (at < 0) {
-                label = 0;
-            } else {
-                label = (int) str.charAt(at);
+
+
+        // adjust buckets number
+        int prev = 0;
+        for (int i = 0; i < 256; i++) {
+            if (buckets[i] > 0) {
+                buckets[i] += prev;
+                prev = buckets[i];
             }
-            counts[label] += 1;
         }
-        for (int i = 1; i < counts.length; i += 1) {
-            counts[i] += counts[i - 1];
+
+
+        // re-order the asciis array in sorted array
+        for (int numString = asciis.length - 1; numString >= 0; numString -= 1) {
+            // int number of character at particular position
+            int num = (int) asciis[numString].charAt(asciis[numString].length() - index - 1);
+
+            // index of that String in sorted array
+            buckets[num] -= 1;
+            int pos = buckets[num];
+
+            // move that String to the new array
+            sorted[pos] = asciis[numString];
+
         }
-        for (int i = unsorted.length - 1; i >= 0; i -= 1) {
-            String str = unsorted[i];
-            int at = index - (len - str.length()) - 1;
-            int label;
-            if (at < 0) {
-                label = 0;
-            } else {
-                label = (int) str.charAt(at);
-            }
-            counts[label] -= 1;
-            int place = counts[label];
-            asciis[place] = str;
-        }
-        sortHelperLSD(asciis, index - 1);
+
+        return sorted;
+
     }
 
     /**
--- lab13/RadixSortTester.java
+++ lab13/RadixSortTester.java
@@ -4,12 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RadixSortTester {
-    private static String[] unsorted = {"123", "765", "34", "968", "932", "23", "1", "85", "41"};
+    private static String[] unsorted = {"ù", " ", "<"};
 
     @Test
     public void testRadixSort() {
-        String[] expected = {"1", "23", "34", "41", "85", "123", "765", "932", "968"};
-        Assert.assertArrayEquals(RadixSort.sort(unsorted), expected);
-//        System.out.print(RadixSort.sort(unsorted)[0]);
+        String[] expected = {"<", " ", "ù"};
+//        Assert.assertArrayEquals(RadixSort.sort(unsorted), expected);
+        System.out.print(RadixSort.sort(unsorted)[1]);
     }
 }
